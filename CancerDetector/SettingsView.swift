ed062f930722d6c827e04e14e30b3775551f8541
prathyet <prathyushyeturi@gmail.com>
2024-08-10 13:48:52 -0400

    Created the tab bar.

---
+++ CancerDetector/SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsView.swift
+//  CancerDetector
+//
+//  Created by Prathyush Yeturi on 8/10/24.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingsView()
+}
---
+++ CancerDetector/StatisticsView.swift
@@ -0,0 +1,18 @@
+//
+//  StatisticsView.swift
+//  CancerDetector
+//
+//  Created by Prathyush Yeturi on 8/10/24.
+//
+
+import SwiftUI
+
+struct StatisticsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StatisticsView()
+}
